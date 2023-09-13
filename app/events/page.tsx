78c7b5979925f7e5c8c707d172cb589d4b4164bb
wzsamuels <wzsamuels@gmail.com>
2023-09-13 09:32:44 -0400

    Add event page

---
+++ app/events/page.tsx
@@ -0,0 +1,26 @@
+import eventImage1 from '@/public/event1.jpg'
+import eventImage2 from '@/public/event2.jpg'
+
+const EventPage = () => {
+  return (
+    <div>
+      <h1>Event Page</h1>
+      Year-Round Programs
+
+Summer Classics Tournament
+RDU Soccer Stars Vs Marsh Creek Bombers
+Pictures 4,5
+
+Summer Soccer Camps for all Ages
+Registrations are open all year round for kids of different ages for 2 weeks camp. Check in for more information.
+Pictures 6,7
+
+TASL Participation Sponsorship
+Triangle Adult Soccer League individual participation sponsorship can be secured on first come first served basis. Contact us for inquiries and application.
+Pictures 8,9
+
+    </div>
+  );
+}
+
+export default EventPage
